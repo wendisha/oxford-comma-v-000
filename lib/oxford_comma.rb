e9aa7d5fbb59239adf99676a48ca8160e889e599
Wendy Calderon <wendycalderon@gmail.com>
2018-06-08 19:00:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  size = array.size
+  if size == 1 
+    conversion = array.join
+  elsif size == 2 
+    conversion = array.join(" and ")
+  else
+    conversion = array.join(", ")
+    sindex = conversion.rindex(",")
+    conversion[sindex] = ", and"
+  end  
+  conversion
 end
