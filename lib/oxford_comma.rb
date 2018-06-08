96f8c95d35f6148169fc3d1be9eddaff4abbf3d9
Wendy Calderon <wendycalderon@gmail.com>
2018-06-08 18:24:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
+
 def oxford_comma(array)
+  size = array.size
+  
+  if size == 2 
+    conversion = array.join(" and ")
+  else
+    conversion = array.join(", ") 
+  end  
+  conversion
+    
+  #array.each do |word|
+   # counter = 0
+    #while counter < size 
+     # conversion = array.join(",")
+      #counter += 1
+    #end
+    #conversion
+ # end 
 
 end
