6bbb6369419ff7406da7c2dc80b1a92417c1f41b
Wendy Calderon <wendycalderon@gmail.com>
2018-06-08 18:59:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,6 +9,7 @@ def oxford_comma(array)
     #new_array = array.insert(size -=1, "and ")
     conversion = new_array.join(", ")
     sindex = conversion.rindex(",")
+    conversion[sindex] = ", and"
   end  
   conversion
     
